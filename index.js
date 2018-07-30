607a45d11e52ae671b4746b33b278ddd5cab6b91
Trevor Blades <tdblades@gmail.com>
2018-07-29 22:33:26 -0700

    Add sans-serif fallback

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import indigo from '@material-ui/core/colors/indigo';
 import {createMuiTheme} from '@material-ui/core/styles';
 
 const title = {
-  fontFamily: "'Helvetica Neue', Helvetica",
+  fontFamily: "'Helvetica Neue', Helvetica, sans-serif",
   fontWeight: 700
 };
 
